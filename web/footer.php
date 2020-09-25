6c1fc99db163d76cf4523513409244e9fe8b3a8e
prbowler <prbowler@yahoo.com>
2020-09-25 15:46:29 -0700

    Added php header and footer

---
+++ web/footer.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p>Created by Philip Bowler</p>";
+
---
+++ web/header.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<div id=\"banner\">
+        <div id=\"logo\">
+            <a href=\"home.php\">
+                <img class=\"logo\" src=\"img/logo.jpg\" alt=\"Logo\" height=\"50\"
+                     width=\"50\">
+            </a>
+        </div>
+        <h1>Philip Bowler CSE 341</h1>
+    </div>";
+
