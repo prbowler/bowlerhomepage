876f171df1a9e7001cf74aff0353155e6b80e6be
prbowler <prbowler@yahoo.com>
2020-09-29 15:54:43 -0700

    Week03 teach03

--- web/teach03.php
+++ web/teach03.php
@@ -5,6 +5,12 @@ Name: <?php echo $_POST["name"]; ?><br>
 mailto: <?php echo $_POST["email"]; ?><br>
 Major: <?php echo $_POST["Major"]; ?><br>
 Comments: <?php echo $_POST["Comments"]; ?><br>
-
+Continents:
+<?php
+   $continents = $_POST["continents"];
+   foreach($continents as $value) {
+       echo "$value <br>";
+   }
+?>
 </body>
 </html>
