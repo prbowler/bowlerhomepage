803e3ff89a2435786146cd25b25552368e077c50
prbowler <prbowler@yahoo.com>
2020-09-29 12:57:40 -0700

    Week03 teach03

--- web/teach03.php
+++ web/teach03.php
@@ -1,9 +1,10 @@
 <html>
 <body>
 
-<?php echo $_POST["name"]; ?><br>
+Name: <?php echo $_POST["name"]; ?><br>
 mailto: <?php echo $_POST["email"]; ?><br>
-
+Major: <?php echo $_POST["Major"]; ?><br>
+Comments: <?php echo $_POST["Comments"]; ?><br>
 
 </body>
 </html>
