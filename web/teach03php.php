5c7b96e64fb25ab6e36119e41c022a6f42a82564
Philip Bowler <prbowler@byui.edu>
2020-09-30 16:59:40 -0700

    Teach03 first

--- web/teach03php.php
+++ web/teach03php.php
@@ -1,10 +1,17 @@
+<?php
+   $name = htmlspecialchars($_POST["name"]);
+   $email = htmlspecialchars($_POST["name"]);
+   $major = htmlspecialchars($_POST["name"]);
+   $comments = htmlspecialchars($_POST["name"]);
+?>
+
 <html>
 
 <body>
-Name: <?php echo $_POST["name"];?> <br>
-Email: <a href="mailto:<?php echo $_POST['email'];?>"><?php echo $_POST['email'];?></a> <br>
-Major: <?php echo $_POST["Major"]; ?> <br>
-Comments: <?php echo $_POST["Comments"]; ?> <br>
+Name: <?php echo $name;?> <br>
+Email: <a href="mailto:<?php echo $email;?>"><?php echo $email;?></a> <br>
+Major: <?php echo $major; ?> <br>
+Comments: <?php echo $comments; ?> <br>
 Continents:
 <?php
      foreach ($_POST['continents'] as $value) {
