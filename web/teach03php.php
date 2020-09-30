fa24cfa0e9ed515252a282a4b95d50334f58dbda
Philip Bowler <prbowler@byui.edu>
2020-09-29 18:52:10 -0700

    Teach03 first

--- web/teach03php.php
+++ web/teach03php.php
@@ -5,7 +5,12 @@ Name: <?php echo $_POST["name"];?> <br>
 Email: <a href="mailto:<?php echo $_POST['email'];?>"><?php echo $_POST['email'];?></a> <br>
 Major: <?php echo $_POST["Major"]; ?> <br>
 Comments: <?php echo $_POST["Comments"]; ?> <br>
-
+Continents:
+<?php
+     foreach ($_POST['continents'] as $value) {
+         echo '$value <br>';
+     }
+?>
 </body>
 </html>
 
