1437b272353da64cdc7c64bba97364335829c158
Philip Bowler <prbowler@byui.edu>
2020-09-29 19:03:06 -0700

    Teach03 first

--- web/teach03.php
+++ web/teach03.php
@@ -8,6 +8,13 @@
    <form action="teach03php.php" method="post">
        Name: <input type="text" name="name"><br>
        E-mail: <input type="email" name="email"><br>
+
+       <?php
+           $major = array("CS-Computer Science","WDD-Web Design and Development","CIT-Computer information Technology","CE-Computer Engineering");
+           foreach ($major as $value) {
+               echo "<input type='radio' name='Major' value='$value'>$value";
+           }
+        ?>
        <input type="radio" name="Major" value="Computer Science"> <label>Computer Science</label><br>
        <input type="radio" name="Major" value="Web Design and Development"> <label>Web Design and Development</label><br>
        <input type="radio" name="Major" value="Computer information Technology"> <label>Computer information Technology</label><br>
