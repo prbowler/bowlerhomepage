76a52c556e9d6f8ccad1fef34de4565c536818b5
Philip Bowler <prbowler@byui.edu>
2020-09-29 19:05:37 -0700

    Teach03 first

--- web/teach03.php
+++ web/teach03.php
@@ -12,13 +12,13 @@
        <?php
            $major = array("CS-Computer Science","WDD-Web Design and Development","CIT-Computer information Technology","CE-Computer Engineering");
            foreach ($major as $value) {
-               echo "<input type='radio' name='Major' value='$value'>$value";
+               echo "<input type='radio' name='Major' value='$value'>$value<br>";
            }
         ?>
-       <input type="radio" name="Major" value="Computer Science"> <label>Computer Science</label><br>
-       <input type="radio" name="Major" value="Web Design and Development"> <label>Web Design and Development</label><br>
-       <input type="radio" name="Major" value="Computer information Technology"> <label>Computer information Technology</label><br>
-       <input type="radio" name="Major" value="Computer Engineering"> <label>Computer Engineering</label><br>
+<!--       <input type="radio" name="Major" value="Computer Science"> <label>Computer Science</label><br>-->
+<!--       <input type="radio" name="Major" value="Web Design and Development"> <label>Web Design and Development</label><br>-->
+<!--       <input type="radio" name="Major" value="Computer information Technology"> <label>Computer information Technology</label><br>-->
+<!--       <input type="radio" name="Major" value="Computer Engineering"> <label>Computer Engineering</label><br>-->
        <textarea name="Comments" rows="10" cols="30"></textarea><br><br>
        <input type="checkbox" name="continents[]" value="North America"><label>North America</label>
        <input type="checkbox" name="continents[]" value="South America"><label>South America</label>
