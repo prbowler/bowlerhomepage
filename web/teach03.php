700ef848e52bf0ae00fe7ed41635ae289b7b7ebd
Philip Bowler <prbowler@byui.edu>
2020-09-29 17:36:43 -0700

    Changed all to php

--- web/teach03.php
+++ web/teach03.php
@@ -7,9 +7,7 @@ Major: <?php echo $_POST["Major"]; ?><br>
 Comments: <?php echo $_POST["Comments"]; ?><br>
 Continents:
 <?php
-   $continent = array($_POST["continents"]);
-   echo count($continent);
-   foreach($continent as $value) {
+   foreach($_POST["continents"] as $value) {
        echo "$value <br>";
    }
 ?>
