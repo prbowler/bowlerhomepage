6ad66887c0cf4fc8700286adf73e074eecc58018
Philip Bowler <prbowler@byui.edu>
2020-10-13 20:45:16 -0700

    ProjectA

--- web/ProjectA/connect-db.php
+++ web/ProjectA/connect-db.php
@@ -1,11 +1,11 @@
 <?php
 try
 {
-    $user = 'prbow';
-    $password = 'database';
-    $db = new PDO('pgsql:host=localhost;dbname=mydb', $user, $password);
+    //$user = 'prbow';
+    //$password = 'database';
+    //$db = new PDO('pgsql:host=localhost;dbname=mydb', $user, $password);
 
-    //$db = new PDO('pgsql:host=localhost;dbname=myTestDB', $user, $password);
+    $db = new PDO('pgsql:host=localhost;dbname=myTestDB', $user, $password);
     // this line makes PDO give us an exception when there are problems,
     // and can be very helpful in debugging! (But you would likely want
     // to disable it for production environments.)
