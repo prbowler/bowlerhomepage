5028486e6ca4a6e9fd09ed072cfde74089c7cab3
Philip Bowler <prbowler@byui.edu>
2020-10-15 13:59:52 -0700

    Added labels

--- web/ProjectA/account.php
+++ web/ProjectA/account.php
@@ -16,10 +16,15 @@
             '
              <form>
              <div id="customer">
-                <input id="username" type="text" value="' . $row["username"] .'"><br>
-                <input id="firstname" type="text" value="' . $row['firstname'] .'">
-                <input id="lastname" type="text" value="' . $row['lastname'] . '"><br>
-                <input id="email" type="text" value="' . $row["email"] . '"><br>
+                <label for="username">Username</label>
+                <input id="username" name="username" type="text" value="' . $row["username"] .'"><br>
+                <label for="firstname">First Name</label>
+                <input id="firstname" name="firstname" type="text" value="' . $row['firstname'] .'">
+                <label for="lastname">Last Name</label>
+                <input id="lastname" name="lastname" type="text" value="' . $row['lastname'] . '"><br>
+                <label for="Email">Email</label>
+                <input id="email" name="email" type="text" value="' . $row["email"] . '"><br>
+                <label for="address">Address</label>
                 <textarea id="address" name="address" rows="4" cols="50">' . $row["address"] . '</textarea><br>
             </div>
             </form>';
