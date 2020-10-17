469f51e8a9e97dc8e895bd04fc1506846e953e1a
Philip Bowler <prbowler@byui.edu>
2020-10-17 11:36:06 -0700

    Add user

--- web/ProjectA/verifyuser.php
+++ web/ProjectA/verifyuser.php
@@ -11,7 +11,7 @@ foreach ($results as $row) {
     if ($row['username'] == $username_submitted && $row['password'] == $password_submitted) {
         $valid = true;
         $user = $row['id'];
-        echo '<script>alert("User has been found ")</script>' . $user;
+        echo '<script>alert("User has been found '. $user . '")</script>';
     }
 }
 
@@ -20,7 +20,7 @@ if ($valid == true) {
         session_start();
     }
     $_SESSION['user'] = $user;
-    echo '<script>alert("User has been changed to ")</script>' . $user;
+    echo '<script>alert("User has been changed to ' . $user . '")</script>';
     include 'home.php';
 } else {
     include 'login.php';
