8bf937fc8bd575334fd46bd1646abf8c21c59db4
Philip Bowler <prbowler@byui.edu>
2020-10-17 16:15:04 -0700

    verify user

--- web/ProjectA/newuser.php
+++ web/ProjectA/newuser.php
@@ -15,7 +15,7 @@
         <input id="firstname" name="firstname" type="text">
         <label for="lastname">Last Name</label>
         <input id="lastname" name="lastname" type="text"><br>
-        <label for="Email">Email</label>
+        <label for="email">Email</label>
         <input id="email" name="email" type="text"><br>
         <label for="address">Address</label>
         <textarea id="address" name="address" rows="4" cols="50"></textarea><br>
--- web/ProjectA/verifynewuser.php
+++ web/ProjectA/verifynewuser.php
@@ -4,6 +4,10 @@ session_start();
 include 'connect-db-ol.php';
 $username_submitted = htmlspecialchars($_POST["username"]);
 $password_submitted = htmlspecialchars($_POST["password"]);
+$firstname = htmlspecialchars($_POST["firstname"]);
+$lastname = htmlspecialchars($_POST["lastname"]);
+$email = htmlspecialchars($_POST["email"]);
+$address = htmlspecialchars($_POST["address"]);
 
 
 $statement = $db->query('SELECT username, password, id FROM shopper');
@@ -17,8 +21,17 @@ foreach ($results as $row) {
     }
 }
 if ($valid === true) {
-    $_SESSION['user'] = $user;
-    //insert into table new user
+    $stmt = $db->prepare('INSERT INTO shopper(username, password, firstname, email, address) VALUES 
+                        (:username_submitted, :password_submitted, :firstname, :lastname, :email, :address);');
+    $stmt->bindValue(':username_submitted', $username_submitted, PDO::PARAM_STR);
+    $stmt->bindValue(':password_submitted', $password_submitted, PDO::PARAM_STR);
+    $stmt->bindValue(':firstname', $firstname, PDO::PARAM_STR);
+    $stmt->bindValue(':lastname', $lastname, PDO::PARAM_STR);
+    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+    $stmt->bindValue(':address', $address, PDO::PARAM_STR);
+    $stmt->execute();
+    $newId = $pdo->lastInsertId('shopper_id_seq');
+    $_SESSION['user'] = $newId;
     include 'home.php';
 } else {
     echo '<script>alert("Username has been used.")</script>';
