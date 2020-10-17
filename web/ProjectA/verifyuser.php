989409451fdbb53da59ac529901d199ffc46904f
Philip Bowler <prbowler@byui.edu>
2020-10-17 11:14:49 -0700

    Add user

--- web/ProjectA/verifyuser.php
+++ web/ProjectA/verifyuser.php
@@ -5,7 +5,14 @@ $password_submitted = htmlspecialchars($_POST["password"]);
 
 $statement = $db->query('SELECT username, password FROM shopper');
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+$valid = false;
 
-echo $results[$username_submitted];
+foreach ($results as $row) {
+    if ($row['username'] == $username_submitted && $row['password'] == $password_submitted) {
+        echo $row['username'];
+        $valid = true;
+        $user = $row['username'];
+    }
+}
 
 ?>
