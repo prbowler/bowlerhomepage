0759b119b31536666cf913ab8f5df71eba0d282c
Philip Bowler <prbowler@byui.edu>
2020-10-24 13:57:29 -0700

    added additem.php

--- web/ProjectA/items.php
+++ web/ProjectA/items.php
@@ -1,17 +1,20 @@
 <?php
-include "connect-db-ol.php";
+
 
 function getItemsByCat($catagoryId)
 {
-    foreach ($db->query('SELECT * FROM items as i WHERE categoryid = '. $catagoryId .'') as $row) {
+    include "connect-db-ol.php";
+
+    foreach ($db->query('SELECT * FROM items as i WHERE categoryid = 1') as $row) {
         $image = 'img/' . $row['image'] . '.jpg';
+        $id = $row['i.id'];
         echo
             '<div class="card">
-                    <img src="' . $image . '"alt="' . $image . '" style="width:100%">
-                    <h3>' . $row["name"] . '</h3>
-                    <p class="price">$' . $row["price"] . '</p>
-                    <p><a href="additem.php">Add to Cart</a></p>
-                </div>';
+                <img src="' . $image .'"alt="'. $image .'" style="width:100%">
+                <h3>' . $row["name"] . '</h3>
+                <p class="price">$' . $row["price"] . '</p>
+                <p><a href="additem.php">Add to Cart</a></p>
+            </div>';
     }
 }
 
