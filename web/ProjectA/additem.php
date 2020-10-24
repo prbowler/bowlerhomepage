5d821f55ff4b06be1e3937f5872bcffb539cf167
Philip Bowler <prbowler@byui.edu>
2020-10-24 14:35:32 -0700

    added items.php

--- web/ProjectA/additem.php
+++ web/ProjectA/additem.php
@@ -3,8 +3,10 @@
     include "connect-db-ol.php";
     $id = $_GET['item_id'];
     echo "id = $id";
-
-    $item = $db->query('SELECT * FROM items WHERE id = ' . $id . '');
+    $query = 'SELECT * FROM items WHERE id = ' . $id .'';
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $item = $stmt->fetchAll(PDO::FETCH_ASSOC);
     var_dump($item);
     /*
     $image = 'img/' . $item['image'] . '.jpg';
