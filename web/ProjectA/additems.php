683f939b3223386f2e1140a31ad11c529e30dadf
Philip Bowler <prbowler@byui.edu>
2020-10-24 15:37:22 -0700

    additems.php

--- web/ProjectA/additems.php
+++ web/ProjectA/additems.php
@@ -4,13 +4,7 @@ $id = htmlspecialchars($_POST['item_id']);
 $quantity = htmlspecialchars($_POST['quantity']);
 $shopperid = 1;
 
-$query = 'SELECT * FROM items WHERE id = ' . $id .'';
-$stmt = $db->prepare($query);
-$stmt->execute();
-$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$item = $items[0];
-
-$stmt = $db->prepare('INSERT INTO cart(item_id, quantity, shopperid) VALUES (:item_id, :quantity, :shopperid);');
+$stmt = $db->prepare('INSERT INTO cart(itemid, quantity, shopperid) VALUES (:item_id, :quantity, :shopperid);');
 $stmt->bindValue(':item_id', $id, PDO::PARAM_INT);
 $stmt->bindValue(':quantity', $quantity, PDO::PARAM_STR);
 $stmt->bindValue(':shopperid', $shopperid, PDO::PARAM_STR);
