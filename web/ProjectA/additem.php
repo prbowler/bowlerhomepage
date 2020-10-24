651958c2b489364d14619317943816eace3c7d03
Philip Bowler <prbowler@byui.edu>
2020-10-24 14:48:27 -0700

    added items.php

--- web/ProjectA/additem.php
+++ web/ProjectA/additem.php
@@ -9,8 +9,8 @@
     $item = $stmt->fetchAll(PDO::FETCH_ASSOC);
     var_dump($item);
     $image = 'img/' . $item['image'] . '.jpg';
-    $name = $item["name"];
-    $price = $item["price"];
+    $name = array($item["name"]);
+    $price = array($item["price"]);
     var_dump($name);
     var_dump($price);
     //echo "Name = $name price = \$$price";
