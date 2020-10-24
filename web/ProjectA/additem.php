f3a1273968a584c206e856b3374729fbaddef656
Philip Bowler <prbowler@byui.edu>
2020-10-24 14:17:42 -0700

    added items.php

--- web/ProjectA/additem.php
+++ web/ProjectA/additem.php
@@ -1,3 +1,16 @@
 <?php
 
-echo 'Item added';
+    include "connect-db-ol.php";
+    $id = $_GET['item_id'];
+    echo "id = $id";
+
+    $item = $db->query('SELECT * FROM items WHERE id = ' . $id . '');
+    $image = 'img/' . $item['image'] . '.jpg';
+    echo
+        '<div class="card">
+                <img src="' . $image .'"alt="'. $image .'" style="width:100%">
+                <h3>' . $item["name"] . '</h3>
+                <p class="price">$' . $item["price"] . '</p>
+                <p><a href="additem.php">Add to Cart</a></p>
+            </div>';
+
