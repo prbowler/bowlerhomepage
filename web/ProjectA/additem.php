d912bb815d9d7d059dfa4edf7510c521791ee69f
Philip Bowler <prbowler@byui.edu>
2020-10-24 15:07:52 -0700

    added items.php

--- web/ProjectA/additem.php
+++ web/ProjectA/additem.php
@@ -1,13 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add Item</title>
+    <link rel="stylesheet" type="text/css" href="card.css">
+</head>
+<body>
+
 <?php
 
     include "connect-db-ol.php";
-    $id = $_GET['item_id'];
+    $id = htmlspecialchars($_GET['item_id']);
     echo "id = $id";
     $query = 'SELECT * FROM items WHERE id = ' . $id .'';
     $stmt = $db->prepare($query);
     $stmt->execute();
     $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    var_dump($items);
+    //var_dump($items);
     $item = $items[0];
     $image = 'img/' . $item['image'] . '.jpg';
     $name = $item['name'];
@@ -18,5 +27,14 @@
                 <h3>' . $name . '</h3>
                 <p class="price">$' . $price . '</p>
             </div>';
+     echo
+        "<form action='additems.php'>
+              <label for='quantity'>Quantity</label>
+              <input type='number' name='quantity'>
+              <input type='submit'>  
+        </form>";
+?>
+</body>
+</html>
 
 
