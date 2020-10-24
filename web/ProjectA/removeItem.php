d58b7989bfc08764bf1b026faf2e1fff379053f5
Philip Bowler <prbowler@byui.edu>
2020-10-24 16:42:49 -0700

    removeItems

--- web/ProjectA/removeItem.php
+++ web/ProjectA/removeItem.php
@@ -11,15 +11,15 @@ require "connect-db-ol.php";
 $id = htmlspecialchars($_GET['item_id']);
 $shopperid = $user;
 
-//$query = 'DELETE FROM cart WHERE itemid = ' . $id . ' AND shopperid = ' . $shopperid . '';
-$stmt = $db->prepare('DELETE FROM cart WHERE itemid = ? AND shopperid = ?');
+$query = 'DELETE FROM cart WHERE itemid = ' . $id . ' AND shopperid = ' . $shopperid . '';
+$stmt = $db->prepare($query);
 //$stmt = $db->prepare('INSERT INTO cart(itemid, quantity, shopperid) VALUES (:item_id, :quantity, :shopperid);');
 //$stmt->bindValue(':item_id', $id, PDO::PARAM_INT);
 //$stmt->bindValue(':quantity', $quantity, PDO::PARAM_STR);
 //$stmt->bindValue(':shopperid', $shopperid, PDO::PARAM_STR);
-$stmt->execute($id, $shopperid);
+$stmt->execute();
 
-$new_page = "home.php";
+$new_page = "cart.php";
 
 header("Location: $new_page");
 die();
