ba4502aeb23646e8c8260739702ec81c75bdc473
Philip Bowler <prbowler@byui.edu>
2020-10-24 15:32:44 -0700

    added additems.php

--- web/ProjectA/additems.php
+++ web/ProjectA/additems.php
@@ -2,14 +2,17 @@
 require "connect-db-ol.php";
 $id = htmlspecialchars($_POST['item_id']);
 $quantity = htmlspecialchars($_POST['quantity']);
+$shopperid = 1;
 
 $query = 'SELECT * FROM items WHERE id = ' . $id .'';
 $stmt = $db->prepare($query);
 $stmt->execute();
 $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $item = $items[0];
-var_dump($item);
-echo "adding items";
-echo $id;
-echo $quantity;
+
+$stmt = $db->prepare('INSERT INTO cart(item_id, quantity, shopperid) VALUES (:item_id, :quantity, :shopperid);');
+$stmt->bindValue(':item_id', $id, PDO::PARAM_INT);
+$stmt->bindValue(':quantity', $quantity, PDO::PARAM_STR);
+$stmt->bindValue(':shopperid', $shopperid, PDO::PARAM_STR);
+$stmt->execute();
 
