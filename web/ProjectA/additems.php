2a2ac1d83d34807c08110a82f756ea1d1352eca2
Philip Bowler <prbowler@byui.edu>
2020-10-24 15:19:57 -0700

    added items.php

--- web/ProjectA/additems.php
+++ web/ProjectA/additems.php
@@ -2,6 +2,13 @@
 
 $itemId = htmlspecialchars($_POST['item_id']);
 $quantity = htmlspecialchars($_POST['quantity']);
+
+$query = 'SELECT * FROM items WHERE id = ' . $id .'';
+$stmt = $db->prepare($query);
+$stmt->execute();
+$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$item = $items[0];
+var_dump($items);
 echo "adding items";
 echo $itemId;
 echo $quantity;
