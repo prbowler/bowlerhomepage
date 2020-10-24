20075c0d5eb9c41ff5b3f9403e1c17f9d86afdfd
Philip Bowler <prbowler@byui.edu>
2020-10-24 16:51:14 -0700

    removeItems

--- web/ProjectA/cart.php
+++ web/ProjectA/cart.php
@@ -21,8 +21,12 @@
                 <img src="' . $image .'"alt="'. $image .'" style="width:100%">
                 <h3>' . $row["name"] . '</h3>
                 <p class="price">$' . $row["price"] . '</p>
-                <p><a href="removeItem.php?item_id='.$itemId.'"> Remove from Cart</a></p>
-                <input class="quantity" value="'.$row["quantity"].'">
+                <p><a href="removeItem.php?item_id='.$itemId.'">Remove from Cart</a></p>
+                <form action="updateItems" method="post">
+                    <input class="quantity" name="quantity" value="'.$row["quantity"].'">
+                    <input type="hidden" name="item_id" value="'.$itemId.'">
+                    <input type="submit" value="Update Cart">
+                </form>
                 <p class="total">Total = $' . $total . '</p>
             </div>';
     }
