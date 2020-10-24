23958f84127dd70e4cca1cddbbbfada8260b10ba
Philip Bowler <prbowler@byui.edu>
2020-10-24 13:35:59 -0700

    added additem.php

--- web/ProjectA/food.php
+++ web/ProjectA/food.php
@@ -13,12 +13,13 @@
 
     foreach ($db->query('SELECT * FROM items as i WHERE categoryid = 1') as $row) {
         $image = 'img/' . $row['image'] . '.jpg';
+        $id = $row['i.id'];
         echo
             '<div class="card">
                 <img src="' . $image .'"alt="'. $image .'" style="width:100%">
                 <h3>' . $row["name"] . '</h3>
                 <p class="price">$' . $row["price"] . '</p>
-                <p><button>Add to Cart</button></p>
+                <p><button onclick="additem.php?item_id=$id">Add to Cart</button></p>
             </div>';
     }
 ?>
