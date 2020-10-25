dfda0d64e8c4c0672f156936cf28d8049de6c958
Philip Bowler <prbowler@byui.edu>
2020-10-24 20:27:38 -0700

    checkout

--- web/ProjectA/purchase.php
+++ web/ProjectA/purchase.php
@@ -2,11 +2,18 @@
 require "connect-db-ol.php";
 include "banner.php";
 
-$shopperid = $_POST[shopper_id];
-$cardNumber = $_POST['card_number'];
+$shopperid = htmlspecialchars($_POST['shopper_id']);
+$cardNumber = htmlspecialchars($_POST['card_number']);
 
 echo "Purchase complete<br>";
 echo "Shopper ID= $shopperid";
 echo "Card Number = $cardNumber";
+if ($cardNumber !== '' && $shopperid !== '') {
+    $query = 'INSERT INTO card(number, shopperid) VALUES (:number, :shopperid)';
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(':number', $cardNumber, PDO::PARAM_INT);
+    $stmt->bindValue(':shopperid', $shopperid, PDO::PARAM_INT);
+    $stmt->execute();
+}
 
 echo "<br><a href='home.php'>Return Home</a>";
