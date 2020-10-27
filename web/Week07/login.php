916b1cb7e67b0cfcb8d65f405446867c3bacab0c
prbowler <prbowler@byui.edu>
2020-10-27 16:10:53 -0700

    Project A

---
+++ web/Week07/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+<?php
+    include "connect-db-ol.php";
+    echo
+    '<form action="verifynewuser.php" method="post">
+         <label for="username">Username</label><br>
+         <input type="text" name="username"><br>
+         <label for="password">Password</label><br>
+         <input type="password" name="password"><br>
+         <input type="submit"><br>
+         <a href="newuser.php">New User</a><br>
+     </form>';
+?>
+</body>
+</html>
---
+++ web/Week07/verifyuser.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+include 'connect-db-ol.php';
+$username_submitted = htmlspecialchars($_POST["username"]);
+$password_submitted = htmlspecialchars($_POST["password"]);
+
+$statement = $db->query('SELECT username, password, id FROM shopper');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+$valid = false;
+$user = 0;
+
+foreach ($results as $row) {
+    if ($row['username'] == $username_submitted && $row['password'] == $password_submitted) {
+        $valid = true;
+        $user = $row['id'];
+    }
+}
+if ($valid === true) {
+    $_SESSION['user'] = $user;
+    include 'home.php';
+} else {
+    echo '<script>alert("Password is incorrect")</script>';
+    include 'login.php';
+}
+
+?>
