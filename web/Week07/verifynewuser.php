28841e4e92e6c54b8db186cea1c09b9f2a0973f8
Philip Bowler <prbowler@byui.edu>
2020-10-27 17:10:41 -0700

    checkout

--- web/Week07/verifynewuser.php
+++ web/Week07/verifynewuser.php
@@ -1,64 +1,33 @@
 <?php
-session_start();
+//session_start();
 
 require "connect-db-ol.php";
-$username = htmlspecialchars($_POST["username"]);
-$password = htmlspecialchars($_POST["password"]);
+require "password.php";
 
-$statement = $db->query('SELECT username, password, id FROM shopper');
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+$username = htmlspecialchars($_POST["username"]);
+$password = password_hash(htmlspecialchars($_POST["password"]));
 $valid = true;
-$user = 0;
+
+/*$statement = $db->query('SELECT username, password, id FROM shopper');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+$user = 0;*/
 //var_dump($results);
 
-foreach ($results as $row) {
+/*foreach ($results as $row) {
     if ($row['username'] == $username) {
         $valid = false;
     }
-}
+}*/
 if ($valid === true) {
-    $query = 'INSERT INTO shopper(username, firstname, lastname, email, address, password) VALUES (:username, :firstname, :lastname, :email, :address, :password)';
+    $query = 'INSERT INTO user_password(username, password) VALUES (:username, :password)';
     $stmt = $db->prepare($query);
     $stmt->bindValue(':username', $username, PDO::PARAM_STR);
-    $stmt->bindValue(':firstname', $firstname, PDO::PARAM_STR);
-    $stmt->bindValue(':lastname', $lastname, PDO::PARAM_STR);
-    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
-    $stmt->bindValue(':address', $address, PDO::PARAM_STR);
     $stmt->bindValue(':password', $password, PDO::PARAM_STR);
     $stmt->execute();
 }
 
-$new_page = "home.php";
+$new_page = "login.php";
 
 header("Location: $new_page");
 die();
 
-/*$statement = $db->query('SELECT username, password, id FROM shopper');
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
-$valid = true;
-$user = 0;
-
-foreach ($results as $row) {
-    if ($row['username'] == $username) {
-        $valid = false;
-    }
-}
-if ($valid === true) {
-    /*$db = get_db();
-    $stmt = $db->prepare('INSERT INTO shopper(username, password, firstname, email, address) VALUES (:username_submitted, :password_submitted, :firstname, :lastname, :email, :address);');
-    $stmt->bindValue(':username_submitted', $username_submitted, PDO::PARAM_STR);
-    $stmt->bindValue(':password_submitted', $password_submitted, PDO::PARAM_STR);
-    $stmt->bindValue(':firstname', $firstname, PDO::PARAM_STR);
-    $stmt->bindValue(':lastname', $lastname, PDO::PARAM_STR);
-    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
-    $stmt->bindValue(':address', $address, PDO::PARAM_STR);
-    $stmt->execute();
-    //include 'verifyuser.php';
-    echo '<script>alert("This function is not working yet. Try username=bbowler password=bbowler")</script>';
-    include 'login.php';
-} else {
-    echo '<script>alert("Username has been used.")</script>';
-    include 'newuser.php';
-}
-
-?>*/
