4cc7ba47184cfdeb01943cad699c273d2a75d22d
Philip Bowler <prbowler@byui.edu>
2020-10-27 17:43:08 -0700

    Team07

--- web/Week07/verifynewuser.php
+++ web/Week07/verifynewuser.php
@@ -9,27 +9,28 @@ $pw = htmlspecialchars($_POST["password"]);
 $password = password_hash('bbowler', PASSWORD_DEFAULT);
 $valid = true;
 
-/*$statement = $db->query('SELECT username, password, id FROM shopper');
+$statement = $db->query('SELECT username, password, id FROM user_password');
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-$user = 0;*/
+$user = 0;
 //var_dump($results);
 
-/*foreach ($results as $row) {
+foreach ($results as $row) {
     if ($row['username'] == $username) {
         $valid = false;
     }
-}*/
+}
+
 if ($valid === true) {
     $query = 'INSERT INTO user_password(username, password) VALUES (:username, :password)';
     $stmt = $db->prepare($query);
     $stmt->bindValue(':username', $username, PDO::PARAM_STR);
     $stmt->bindValue(':password', $password, PDO::PARAM_STR);
     $stmt->execute();
-}
+    echo '<script>alert("User Added")';
+} else { echo '<script>alert("Username is already used")';}
 
-echo "End";
-/*$new_page = "login.php";
+$new_page = "login.php";
 
 header("Location: $new_page");
 die();
-*/
+
