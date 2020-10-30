44d41d410590e4ab794ce4f90ff2522c089ae761
prbowler <prbowler@byui.edu>
2020-10-30 11:32:19 -0700

    Project A

--- web/ProjectA/banner.php
+++ web/ProjectA/banner.php
@@ -7,6 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <title>Banner</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="banner.css">
 </head>
 <body>
@@ -33,6 +34,7 @@ if ($user === 0) {
     </div>
     <div>
         <h1 id="store-name">PB Survival Supplies</h1>
+        <i class="fa fa-bars"></i>
         <ul id="main-menu" class="right">
             <li><a href="home.php">Home</a></li>
             <?php echo $accountLink; ?>
