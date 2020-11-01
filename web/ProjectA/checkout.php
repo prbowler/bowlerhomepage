a1cbfda7720caa3b83c32531a7dbd30494f521f8
Philip Bowler <prbowler@byui.edu>
2020-10-31 19:59:50 -0700

    ProjectA

--- web/ProjectA/checkout.php
+++ web/ProjectA/checkout.php
@@ -12,6 +12,14 @@ include "banner.php";
 include "connect-db-ol.php";
 require "get_cart.php";
 
+$user = 0;
+if (isset($_SESSION['user'])) {
+    $user = $_SESSION['user'];
+} else {
+    $new_page = "login.php";
+    header("Location: $new_page");
+}
+
 $cart = getCart($db, $user);
 echo "<table><tr><th>Name</th><th>Price</th><th>Quantity</th><th>Total</th></tr>";
 foreach ($db->query('SELECT * FROM items as i JOIN cart_details as cd on i.id = cd.itemid WHERE cd.cartid = ' . $cart . '') as $row) {
