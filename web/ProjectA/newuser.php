e40a825c6b1fbc7191402c87a77351e04449a6c0
Philip Bowler <prbowler@byui.edu>
2020-10-31 18:24:59 -0700

    ProjectA

--- web/ProjectA/newuser.php
+++ web/ProjectA/newuser.php
@@ -12,17 +12,17 @@
      <form action="verifynewuser.php" method="post">
      <h2>Account information</h2>
      <div id="customer">
-        <label for="username">Username</label>
+        <label for="username" class="label">Username</label>
         <input id="username" name="username" type="text"><br>
-        <label for="password">Password</label>
+        <label for="password" class="label">Password</label>
         <input id="password" name="password" type="password"><br>
-        <label for="firstname">First Name</label>
-        <input id="firstname" name="firstname" type="text">
-        <label for="lastname">Last Name</label>
+        <label for="firstname" class="label">First Name</label>
+        <input id="firstname" name="firstname" type="text"><br>
+        <label for="lastname" class="label">Last Name</label>
         <input id="lastname" name="lastname" type="text"><br>
-        <label for="email">Email</label>
+        <label for="email" class="label">Email</label>
         <input id="email" name="email" type="text"><br>
-        <label for="address">Address</label>
+        <label for="address" class="label">Address</label>
         <textarea id="address" name="address" rows="4" cols="50"></textarea><br>
         <input id="submit" type="submit">
      </div>
