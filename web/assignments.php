df3f77c919916c69b012da663ae9a57beaae91ce
prbowler <prbowler@byui.edu>
2022-04-05 12:11:35 -0700

    fixed links

--- web/assignments.php
+++ web/assignments.php
@@ -2,11 +2,11 @@
 <html lang="en-US">
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="assignments for CSE 341">
+    <meta name="description" content="projects">
     <meta name="keywords" content="links">
     <meta name="author" content="Philip Bowler">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>"Links to CSE 341 Assignments"</title>
+    <title>"Links to Projects"</title>
     <link rel="stylesheet" type="text/css" href="myStyle.css">
     <script src="assignJs.js"></script>
 </head>
@@ -14,30 +14,15 @@
 <header>
     <div id="banner">
        <?php include 'logo.php';?>
-        <h1>Philip Bowler's CSE 341 Assignments</h1>
+        <h1>Philip Bowler's Projects</h1>
     </div>
 </header>
 <section id="main">
     <div id="assignments">
-        <h2>CSE 341 Work</h2>
+        <h2>Projects</h2>
         <ul>
-            <li><a href="helloworld.php">Week01</a></li>
-            <li><a href="week02.php">Week02</a></li>
-            <li><a href="t3.php">Week03 Teach</a></li>
-            <li><a href="Week03/Ponder/shopping.php">Week03 Ponder</a></li>
-            <li><a>Week04</a></li>
-            <li><a href="ProjectA/home.php">Week05</a></li>
-            <li><a href="./Week06/team06.php">Team Week06</a></li>
-            <li><a href="./Week07/team07.php">Team Week07</a></li>
-            <li><a>Week08</a></li>
-            <li><a>Week09</a></li>
-            <li><a>Week10</a></li>
-            <li><a>Week11</a></li>
-            <li><a>Week12</a></li>
-            <li><a>Week13</a></li>
-            <li><a>Week14</a></li>
-            <li><a href="./ProjectA/home.php">Project A</a></li>
-            <li>Project B</li>
+            <li><a href="Week03/Ponder/shopping.php">PB Groceries</a></li>
+            <li><a href="./ProjectA/home.php">PB Survival Supplies</a></li>
         </ul>
     </div>
 </section>
