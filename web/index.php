82c03e87973f38e85d8af74f3526dcbf3715867d
prbowler <prbowler@byui.edu>
2022-04-07 12:02:11 -0700

    fixes on homepage

--- web/index.php
+++ web/index.php
@@ -18,6 +18,8 @@
     </div>
 </header>
 <section id="main">
+    <h2> Hi my name is Philip Bowler</h2>
+    <h3> I enjoy Web Development both front-end and back-end.</h3> 
     <div id="picture">
         <figure>
             <img src="img/me.jpg" alt="My wife and I" width="173" height="164" id="me">
@@ -29,7 +31,13 @@
         </div>
     </div>
     <div>
-        <a href="assignments.php">Projects</a>
+        <h2>Here are some of my projects</h2>
+        <ul>          
+            <li><a href="https://bowlerhomeautomation.herokuapp.com/">A home automation system built with PHP, JavaScript, Python, CSS, HTML, and Mysql</a></li>
+            <li><a href="./ProjectA/home.php">A store front built with PHP, JavaScript, CSS, and HTML</a></li>
+            <li><a href="https://projectrook.herokuapp.com/">A Rook game built with Node.js, EJS, and PosgreSQL</a></li>
+            <li><a href="https://bowlerzengarden.herokuapp.com/">ZenGarden styling using CSS</a></li>
+        </ul>
     </div>
 </section>
 <footer>
